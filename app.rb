442c3dcfcb7f1bbab371dc9bcc64d5f5e7f27424
Donna Amos <donnacamos.developer@gmail.com>
2019-07-03 02:25:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,6 +24,7 @@ class App < Sinatra::Base
     @word3 = params[:word3]
     @word4 = params[:word4]
     @word5 = params[:word5]
+    erb :saywords 
   end 
   
   get '/:operation/:number1/number2'
