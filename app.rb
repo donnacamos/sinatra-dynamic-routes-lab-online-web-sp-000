4c7563efc19e437a785a5b2794e15a5337887ab1
Donna Amos <donnacamos.developer@gmail.com>
2019-07-03 02:34:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,36 +2,44 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get '/reversename/:name' do
+ get "/reversename/:name" do
     @user_name = params[:name]
-    "#{@user_name.reverse}" 
+    "#{@user_name.reverse}"
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get "/say/:number/:phrase" do
+    @phrase = params[:phrase]
+    @number = params[:number].to_i
+
+    result = ""
+    @number.times {result << @phrase+"\n"}
+    result
   end
 
   get '/square/:number' do
-    @num = params[:number].to_i
-    erb :square
-  end
-
-  get '/say/:number/:phrase' do
     @number = params[:number].to_i
-    @phrase = params[:phrase]
-    erb :say
+    "#{@number * @number}"
   end
 
-  get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @w1 = params[:word1]
-    @w2 = params[:word2]
-    @w3 = params[:word3]
-    @w4 = params[:word4]
-    @w5 = params[:word5]
-    erb :saywords
-  end
-
-  get '/:operation/:number1/:number2' do
+  get "/:operation/:num1/:num2" do
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
     @operation = params[:operation]
-    @num1 = params[:number1].to_i
-    @num2 = params[:number2].to_i
-    erb :operation
+    case @operation
+    when 'add'
+      "#{@num1 + @num2}"
+    when 'multiply'
+      "#{@num1 * @num2}"
+    when 'subtract'
+      "#{@num1 - @num2}"
+    when 'divide'
+      "#{@num1 / @num2}"
+    end
+
   end
 
 end
