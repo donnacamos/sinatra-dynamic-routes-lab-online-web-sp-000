63e60e3def147301ecdb2be158c7a871cc204d28
Donna Amos <donnacamos.developer@gmail.com>
2019-07-03 02:18:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,5 +5,13 @@ class App < Sinatra::Base
   get '/reversename/:name' do 
     @name = params[:name]
   end 
+  
+  get '/square/:number' do 
+  end 
+  
+  get '/say/:number/:phrase' do 
+    @number = params[:number]
+    @phrase = params[:phrase] 
+  end 
 
 end
