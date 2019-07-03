b06c7c2894a54ac6ec6de0d615da2d66e58d75aa
Donna Amos <donnacamos.developer@gmail.com>
2019-07-03 02:24:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,8 @@ class App < Sinatra::Base
   end 
   
   get '/square/:number' do 
-    @number = params[:number] 
+    @number = params[:number].to_s  
+    
   end 
   
   get '/say/:number/:phrase' do 
